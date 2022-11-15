1cb2dc59567859ef3b32af5bed84ee9a1b555d99
aleksandr cherkasin <aleksmail1box@gmail.com>
2022-11-15 21:10:09 +0300

    Editing help information

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,6 +3,13 @@
 import { Command } from 'commander';
 const program = new Command();
 
-program.name('gendiff').description('Compares two configuration files and shows a difference.').version('1.0.0');
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0');
 
-program.parse();
+program
+  .arguments('<filepath1> <filepath2>')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format <type>', 'output format')
+  .parse(process.argv);
